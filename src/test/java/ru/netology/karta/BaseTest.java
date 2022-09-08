6ae547cf21055cac45fa117289f8066d9a45e023
ekat1983 <shumkovakatya@yandex.ru>
2022-09-08 21:56:31 +0300

    karta

--- src/test/java/ru/netology/karta/BaseTest.java
+++ src/test/java/ru/netology/karta/BaseTest.java
@@ -25,9 +25,8 @@ public class BaseTest {
     @Test
     void Test() {
         driver.get("http://localhost:9999");
-        List<WebElement>elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Иванова Мария");
-        elements.get(1).sendKeys("+79914567890");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Иванова Мария");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79914567890");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button_view_extra")).click();
         String text = driver.findElement(By.className("paragraph")).getText();
@@ -41,7 +40,6 @@ public class BaseTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999");
     }
 
     @AfterEach
