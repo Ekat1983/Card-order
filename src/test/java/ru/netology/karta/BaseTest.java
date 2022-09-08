20dea274e1a6eda72bf4a602546b4cb44aaa1ad0
ekat1983 <shumkovakatya@yandex.ru>
2022-09-08 13:10:19 +0300

    karta

--- src/test/java/ru/netology/karta/BaseTest.java
+++ src/test/java/ru/netology/karta/BaseTest.java
@@ -3,9 +3,15 @@ package ru.netology.karta;
 import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BaseTest {
 
@@ -19,7 +25,13 @@ public class BaseTest {
     @Test
     void Test() {
         driver.get("http://localhost:9999");
-        System.out.println();
+        List<WebElement>elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иванова Мария");
+        elements.get(1).sendKeys("+79914567890");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button_view_extra")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
     @BeforeEach
